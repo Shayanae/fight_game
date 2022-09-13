4ab9a03fbaae4a338efcf4f091ba6ab4c26796ca
Fitamant Quentin <quentinfitamantp@gmail.com>
2022-09-13 15:03:59 +0200

    Ajout des mouvements des personnages

--- script/main.js
+++ script/main.js
@@ -4,14 +4,18 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
+// génére le canvas
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2;
+const gravity = 0.7;
+
+// génére les blocs personnages
 class Sprite {
     constructor({position, velocity}){
         this.position = position;
         this.velocity = velocity;
-        this.height = 150
+        this.height = 150;
+        this.lastKey;
     }
 
     draw(){
@@ -21,6 +25,8 @@ class Sprite {
 
     update(){
         this.draw();
+
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height){
@@ -31,6 +37,7 @@ class Sprite {
     }
 }
 
+// Joueur 1
 const player = new Sprite({
     position: {
     x: 0,
@@ -42,6 +49,7 @@ const player = new Sprite({
     }
 })
 
+// Joueur 2
 const enemy = new Sprite({
     position: {
         x: 400,
@@ -55,12 +63,100 @@ const enemy = new Sprite({
 
 console.log(player);
 
+// Entré clavier
+const keys = {
+    q: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
+
+// Génére l'animation
 function animate(){
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
+
+    player.velocity.x = 0;
+    enemy.velocity.x = 0;
+
+    // player movements
+    if (keys.q.pressed && player.lastKey === 'q'){
+        player.velocity.x = -5;
+    } else if (keys.d.pressed && player.lastKey === 'd'){
+        player.velocity.x = 5;
+    }
+
+    // enemy movements
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){
+        enemy.velocity.x = -5;
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
+        enemy.velocity.x = 5;
+    }
 }
 
-//animate();
+animate();
+
+// gére les entrées clavier
+window.addEventListener('keydown', (event) => {
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = true;
+            player.lastKey = 'd';
+            break;
+        case 'q':
+            keys.q.pressed = true;
+            player.lastKey = 'q';
+            break;
+        case 'z':
+            player.velocity.y = -20;
+            break;
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            enemy.lastKey = 'ArrowRight';
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            enemy.lastKey = 'ArrowLeft';
+            break;
+        case 'ArrowUp':
+            enemy.velocity.y = -20;
+            break;
+    }
+    console.log(event.key);
+})
+
+// gére les relachement des touches
+window.addEventListener('keyup', (event) => {
+    // player keys
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 'q':
+            keys.q.pressed = false;
+            break;
+    }
+
+    // enemy keys
+    switch(event.key){
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break;
+    }
+    console.log(event.key);
+})
