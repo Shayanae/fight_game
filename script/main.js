48d19235c094e1d4e640f4fdb7b7cd0fd17c3757
Fitamant Quentin <quentinfitamantp@gmail.com>
2022-09-15 15:36:23 +0200

    Arret à 1:10:43

--- script/main.js
+++ script/main.js
@@ -11,21 +11,47 @@ const gravity = 0.7;
 
 // génére les blocs personnages
 class Sprite {
-    constructor({position, velocity}){
+    constructor({position, velocity, color = 'red', offset}){
         this.position = position;
         this.velocity = velocity;
+        this.width = 50;
         this.height = 150;
         this.lastKey;
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color;
+        this.isAttacking;
     }
 
     draw(){
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, this.height);
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        // attack box
+        if(this.isAttacking){
+            c.fillStyle = 'green';
+            c.fillRect(
+                this.attackBox.position.x, 
+                this.attackBox.position.y, 
+                this.attackBox.width, 
+                this.attackBox.height);
+        }    
     }
 
     update(){
         this.draw();
+        // Update attack position
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+        this.attackBox.position.y = this.position.y;
 
+        // Apply mouvement to caracter
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
@@ -34,6 +60,13 @@ class Sprite {
         } else {
             this.velocity.y += gravity;
         }
+    }
+
+    attack(){
+        this.isAttacking = true;
+        setTimeout(() => {
+            this.isAttacking = false;
+        }, 100)
     }
 }
 
@@ -44,6 +77,10 @@ const player = new Sprite({
     y: 0
     },
     velocity: {
+        x: 0,
+        y: 0
+    },
+    offset: {
         x: 0,
         y: 0
     }
@@ -57,6 +94,11 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
+        y: 0
+    },
+    color : 'blue',
+    offset: {
+        x: -50,
         y: 0
     }
 })
@@ -79,7 +121,16 @@ const keys = {
     }
 }
 
-// Génére l'animation
+function rectangularCollision({rectangle1, rectangle2}){
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
+        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
+        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
+
+// Generate animation
 function animate(){
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
@@ -103,11 +154,31 @@ function animate(){
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
         enemy.velocity.x = 5;
     }
+
+    // Detect for collision
+    // Joueur
+    if (rectangularCollision({
+        rectangle1: player,
+        rectangle2: enemy
+        })
+        && player.isAttacking){
+            player.isAttacking = false;
+            console.log('go');
+    }
+    // Enemy
+    if (rectangularCollision({
+        rectangle1: enemy,
+        rectangle2: player
+        })
+        && enemy.isAttacking){
+            enemy.isAttacking = false;
+            console.log('enemy attack successful');
+    }
 }
 
 animate();
 
-// gére les entrées clavier
+// take key event
 window.addEventListener('keydown', (event) => {
     switch(event.key){
         case 'd':
@@ -121,6 +192,9 @@ window.addEventListener('keydown', (event) => {
         case 'z':
             player.velocity.y = -20;
             break;
+        case ' ':
+            player.attack();
+            break;
 
         case 'ArrowRight':
             keys.ArrowRight.pressed = true;
@@ -133,11 +207,14 @@ window.addEventListener('keydown', (event) => {
         case 'ArrowUp':
             enemy.velocity.y = -20;
             break;
+        case 'ArrowDown':
+            enemy.isAttacking = true;
+            break;
     }
     console.log(event.key);
 })
 
-// gére les relachement des touches
+// manage key release
 window.addEventListener('keyup', (event) => {
     // player keys
     switch(event.key){
